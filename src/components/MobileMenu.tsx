b237c6228e8a94e5779c7a11cb21f8af263288ba
root <muchellealvin@gmail.com>
2025-06-01 00:47:55 +0300

    update dark mode foreground text

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -103,7 +103,7 @@ export const MobileMenu: React.FC<MobileMenuProps> = ({
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full bg-[rgba(10,10,10,0.8)] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out md:hidden ${
+      className={`fixed top-0 left-0 w-full bg-[rgba(8,17,32,0.8)] z-40 flex flex-col items-center justify-center transition-all duration-300 ease-in-out md:hidden ${
         menuOpen
           ? "h-screen opacity-100 pointer-events-auto"
           : "h-0 opacity-0 pointer-events-none"
